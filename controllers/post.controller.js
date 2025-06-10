32a36ad7a4042709c6697bc521c3f384646e85f0
MayorOla4Dev <jossey5066@icloud.com>
2025-06-10 19:36:00 +0100

    update

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,7 +1,14 @@
 const postModel = require("../models/post.model");
 
 const createPost = async (req, res) => {
-	return res.send("Making post");
+	const body = req.body;
+	try {
+		const newPost = new postModel(body);
+		const savedPost = await newPost.save();
+		return res.json(savedPost);
+	} catch (error) {
+		return res.send("something went wrong");
+	}
 };
 
 module.exports = { createPost };
